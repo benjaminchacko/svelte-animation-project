c5961543e4e8f32a9946b893cfca8292a850b7fd
Benjamin Chacko <benjaminchacko@outlook.com>
2021-04-24 16:15:15 -0600

    removed box and added text

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,42 @@
 <script>
-	let isActive = true;
+	let isHidden = true;
 </script>
 
 <main>
 	<h1>Svelte Animation Project</h1>
 
-	<button on:click={() => (isActive = !isActive)}>
-		{#if isActive}Hide Box{:else}Show Box{/if}
+	<button on:click={() => (isHidden = !isHidden)}>
+		{#if isHidden}Show Text{:else}Hide Text{/if}
 	</button>
-	{#if isActive}
-		<div class="box" />
+	{#if !isHidden}
+		<div>
+			<p>
+				Gingerbread jujubes fruitcake lollipop. Halvah cotton candy
+				muffin jelly-o. Jelly beans soufflé chocolate bar bear claw
+				fruitcake biscuit lemon drops apple pie. Pastry caramels halvah
+				donut. Sesame snaps cheesecake jelly-o cheesecake bonbon. Dragée
+				gingerbread sweet apple pie topping gummies halvah powder
+				tootsie roll. Oat cake cookie pastry croissant. Marshmallow
+				topping lollipop. Pie fruitcake liquorice soufflé cake sesame
+				snaps macaroon. Dragée donut sesame snaps fruitcake jelly-o
+				gummi bears pie. Biscuit bonbon danish cotton candy wafer icing
+				pie. Jelly pudding cake dragée brownie pudding soufflé. Icing
+				sweet sweet roll chocolate bar powder. Cake caramels cake icing.
+				Jelly beans toffee chocolate cake cake. Icing gummi bears donut
+				cookie ice cream.
+			</p>
+			<p>
+				Icing cake muffin wafer. Macaroon chupa chups bear claw.
+				Macaroon tootsie roll biscuit pie dessert jelly-o. Cupcake ice
+				cream powder biscuit danish gingerbread pudding macaroon.
+				Dessert sweet roll lollipop marshmallow. Biscuit soufflé biscuit
+				chocolate tootsie roll lemon drops muffin macaroon donut. Icing
+				carrot cake topping toffee caramels apple pie sesame snaps.
+				Sweet roll ice cream tootsie roll. Carrot cake tiramisu
+				marshmallow cookie fruitcake. Sweet apple pie fruitcake pudding
+				pudding.
+			</p>
+		</div>
 	{/if}
 </main>
 
@@ -26,14 +53,6 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	.box {
-		background: red;
-		display: block;
-		height: 100px;
-		margin: 0 auto;
-		width: 100px;
 	}
 
 	@media (min-width: 640px) {
